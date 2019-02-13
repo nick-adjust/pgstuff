d9161787b5057293c01e892a08025e087072dd10
nickb <nick-adjust@github.com>
2019-02-13 12:10:46 +0100

    Added more comments and some verbosity to warning

--- change_tablespace_with_size.sql
+++ change_tablespace_with_size.sql
@@ -3,7 +3,7 @@ create or replace function change_tablespace_with_size
     _src_schema name,
     _src_spc    name,
     _dst_spc    name,
-    _size       int8 default 2199023255552 /* number of bytes to move */
+    _size       int8 default int8'2'*1024*1024*1024*1024 /* number of bytes to move */
 )
     returns int8 /* number of bytes actually moved */
     language plpgsql volatile as
@@ -15,6 +15,9 @@ declare
     _result         int8:=0;
     _relspcoid_src  int4; /* this is int4, because it can be 0 which is not very nice for an oid */
     _ddl            text;
+    _err_sqlstate   text;
+    _err_message    text;
+    _err_detail       text;
 begin
     /* Get source tablespace id for pg_class. pg_default is always 0
      * Throws an error if there is no such tablespace (INTO STRICT)
@@ -57,41 +60,53 @@ begin
                 relname,
                 relsize,
                 format('ALTER TABLE %I.%I SET TABLESPACE %I;', _src_schema, relname, _dst_spc)
-        from
-        (
-        select
-                relname,
-                relsize,
-                relkind,
-                reldate,
-                sum(relsize) over (order by reldate asc)  running_sum
             from
             (
                 select
                         relname,
+                        relsize,
                         relkind,
-                        pg_relation_size(c.oid) relsize,
-                        substring(relname::text,'_(\d+_\d+_\d+)')::date reldate
-                    from pg_class c
-                    where
-                            true
-                        and c.relkind = any (array['r', 'i'])
-                        and c.reltablespace = _relspcoid_src
-                        and relnamespace = _relnamespace
-            ) rels
-        ) a
+                        reldate,
+                        sum(relsize) over (order by reldate asc)  running_sum
+                    from
+                    (
+                        select
+                                relname,
+                                relkind,
+                                pg_relation_size(c.oid) relsize,
+                                substring(relname::text,'_(\d+_\d+_\d+)')::date reldate
+                            from pg_class c
+                            where
+                                    true
+                                and c.relkind = any (array['r', 'i'])
+                                and c.reltablespace = _relspcoid_src
+                                and relnamespace = _relnamespace
+                    ) rels
+            ) a
         where running_sum <= _size
         order by reldate asc
     ) loop
         raise notice 'About to process %.%',
             quote_ident(_src_schema), quote_ident(_relname);
         _result = _result + _relsize;
+        /*
+         * We run the actual ddl inside a subtransaction. This way a failure in
+         * a single statement does not result in a rollback and we can carry on
+         * with the rest of tables.
+         */
         begin
             execute _ddl;
         exception when others then
-            raise warning 'WARNING: Failed to move %.%',
-                quote_ident(_src_schema), quote_ident(_relname);
-            continue;
+            get stacked diagnostics _err_sqlstate = RETURNED_SQLSTATE;
+            get stacked diagnostics _err_message = MESSAGE_TEXT;
+            get stacked diagnostics _err_detail = PG_EXCEPTION_DETAIL;
+            raise warning 'Failed to move %.%. SQLSTATE %',
+                quote_ident(_src_schema), quote_ident(_relname), _err_sqlstate
+                using
+                    detail = format('%sMessage: "%s" %sDetail: "%s"',
+                                     E'\n\t', _err_message, E'\n\t', _err_detail);
+            continue; /* move to the next item without outputting "Done" message */
+
         end;
 
         raise notice 'Done with %.%',
@@ -100,3 +115,7 @@ begin
     return _result;
 end;
 $fnc$;
+
+/*
+select * from change_tablespace_with_size('public', 'pg_default', 'spc');
+*/
